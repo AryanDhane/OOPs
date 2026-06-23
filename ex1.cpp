97ea94da712e2afad7586a53fb4751a97c24eabb
AryanDhane <dhanearyan150074@gmail.com>
2026-06-23 16:48:06 +0530

    ..

--- ex1.cpp
+++ ex1.cpp
@@ -7,5 +7,14 @@ class teacher {
         string name;
         string subject;
         double salary;
-
 };
+
+int main() {
+    teacher t1;
+
+    t1.name = "aryan";
+
+    cout << t1.name << endl;
+
+    return 0;
+}

4fa6142b81df1f24e24d10673b6e8d28afb165ea
AryanDhane <dhanearyan150074@gmail.com>
2026-06-23 21:20:35 +0530

    ..

--- ex1.cpp
+++ ex1.cpp
@@ -8,18 +8,17 @@ class teacher {
         string subject;
         double salary;
 
-        int getinfo(name,subject,salary){
+    int getinfo(name,subject,salary){
+       this-> name = name;
+        this-> subject = subject;
+        this-> salary = salary;
     return;
 }
 };
 
 int main() {
     teacher t1;
-
     t1.getinfo("aryan","english",1000);
   
-    cout << t1.name << endl;
-
-
     return 0;   
 }
